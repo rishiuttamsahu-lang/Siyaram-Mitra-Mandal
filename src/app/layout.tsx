ae434576923c1232369d8d9e142740f78e425657
Rishi Sahu <rishiuttamsahu@gmail.com>
2026-05-21 11:46:59 +0530

    Add Microsoft site verification meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,6 +105,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
         <meta name="google-site-verification" content="8qtmD0p7C0NyIPpXQpFyeO-w_TzzLnLPycMB66SB40M" />
+        <meta name="msvalidate.01" content="52C2C02AE9C5F23CD3542A219B24CA3F" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
