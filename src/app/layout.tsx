b824209163ae840f7786a3c1a9c4bf7204902a3f
Rishi Sahu <rishiuttamsahu@gmail.com>
2026-05-21 11:46:07 +0530

    Add Google site verification meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,6 +104,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Gotu&family=Rozha+One&display=swap"
           rel="stylesheet"
         />
+        <meta name="google-site-verification" content="8qtmD0p7C0NyIPpXQpFyeO-w_TzzLnLPycMB66SB40M" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
